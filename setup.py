ca66cdd5dbe5ce566b50960e4fb8f6347d50510d
Christoph Möhl <christoph.moehl@dzne.de>
2019-03-28 13:56:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+requirements_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+install_reqs = parse_requirements(requirements_txt, session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+def readme():
+    README_md = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(README_md) as f:
+        return f.read()
+
+
+ns = os.environ.get('CI_PROJECT_NAMESPACE', 'idaf')
+
+setup(name='yapic-cli',
+      version='0.0.0',
+      description='Yet another Pixel Classifier (based on deep learning)',
+      long_description=readme(),
+      author='Manuel Schoelling, Christoph Moehl',
+      author_email='manuel.schoelling@gmx.de, christoph.moehl@dzne.de',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      dependency_links=[
+          'git+http://animate-x3.dzne.ds/{}/yapic.git@master#egg=yapic-0.0.0'.format(ns),
+          'git+http://animate-x3.dzne.ds/{}/yapic_io.git@master#egg=yapic_io-0.1.0'.format(ns),
+      ],
+      zip_safe=False,
+      install_requires=reqs,
+      test_suite='nose.collector',
+      entry_points={
+          'console_scripts': ['yapic=yapic_cli.main:entry_point'],
+      },
+      tests_require=['nose', 'coverage', 'nose-timer', 'nose-deadline'])
---
+++ yapic/tests/test_main.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestMain(TestCase):
+
+    def test_tmp(self):
+        self.assertTrue(True)
