93ac4d981295fccdecb252ab19353f0353f63509
moehlc <christoph.moehl@dzne.de>
2020-03-23 16:08:21 +0100

    added tests and unet_2d check for DeepImageJExporter

--- yapic/deepimagej_exporter.py
+++ yapic/deepimagej_exporter.py
@@ -17,7 +17,31 @@ class DeepimagejExporter(object):
     '''
 
     def __init__(self, model_path, save_path, example_image_path):
-        assert os.path.isdir(save_path)
+
+        save_dir = os.path.dirname(save_path)
+        assert os.path.isdir(save_dir), '{} does not exist'.format(save_dir)
         self.s = Session()
         self.s.load_prediction_data(example_image_path, 'some/path')
         self.s.load_model(model_path)
+
+        msg = 'model is not unet_2d, cannot be exported to deepimagej'
+        assert self.is_model_unet_2d(), msg
+
+    def is_model_unet_2d(self):
+        return self.s.model.count_params() == 32424323
+
+    def reshape_unet_2d(self, size='middle'):
+
+        if size=='small':
+            shape_xy = 112
+        if size=='middle':
+            shape_xy = 224
+        if size=='large':
+            shape_xy = 448
+        else:
+            shape_xy = 112
+
+        
+        model_reshaped = unet_2d.build_network(N_classes,
+                                              (N_channels, 1, small_xy, small_xy),
+                                               squeeze=True, padding='same')
--- yapic/tests/test_exporter.py
+++ yapic/tests/test_exporter.py
@@ -6,7 +6,33 @@ import os
 base_path = os.path.dirname(__file__)
 
 
-def train_test_model_1():
+def train_test_model_unet_2d():
+    img_path = os.path.join(
+        base_path,
+        '../test_data/shapes/pixels/*')
+    label_path = os.path.join(
+        base_path,
+        '../test_data/shapes/labels.ilp')
+    savepath = os.path.join(
+        base_path,
+        '../test_data/tmp')
+
+    os.makedirs(savepath, exist_ok=True)
+
+    t = Session()
+    t.load_training_data(img_path, label_path)
+    t.make_model('unet_2d', (1, 220, 220))
+
+    model_filename = os.path.join(savepath, 'model_unet_2d.h5')
+    t.train(max_epochs=2,
+            steps_per_epoch=2,
+            log_filename=os.path.join(savepath, 'log.csv'),
+            model_filename=model_filename)
+
+    return model_filename
+
+
+def train_test_model_convnet():
     img_path = os.path.join(
         base_path,
         '../test_data/shapes/pixels/*')
@@ -23,31 +49,65 @@ def train_test_model_1():
     t.load_training_data(img_path, label_path)
     t.make_model('convnet_for_unittest', (1, 100, 100))
 
-    model_filename = os.path.join(savepath, 'model.h5')
+    model_filename = os.path.join(savepath, 'model_convnet.h5')
     t.train(max_epochs=2,
-            steps_per_epoch=4,
+            steps_per_epoch=2,
             log_filename=os.path.join(savepath, 'log.csv'),
             model_filename=model_filename)
 
     return model_filename
 
 
-
-
-
 class TestDeepimagejExporter(TestCase):
 
-    def test_init_deepimagej_exporter(self):
+    @classmethod
+    def setUpClass(cls):
+        model_path = train_test_model_unet_2d()
+        print('saved unet_2d as {}'.format(model_path))
+
+        # model_path = train_test_model_convnet()
+        # print('saved unet_2d as {}'.format(model_path))
+
+    def test_is_model_unet_2d(self):
 
         example_image_path = os.path.abspath(os.path.join(
             base_path,
             '../test_data/shapes/pixels/pixels_1.tiff'))
-        
-        model_path = train_test_model_1()
+
+        save_path = os.path.abspath(os.path.join(
+            base_path,
+            '../test_data/tmp/exported_model'))
+
+        model_path = os.path.join(
+            base_path,
+            '../test_data/tmp/model_unet_2d.h5')
+
         print('model_path: {}'.format(model_path))
-        save_path = ''
 
         exp = DeepimagejExporter(model_path,
                                  save_path,
                                  example_image_path)
-        assert False
+
+        assert exp.is_model_unet_2d()
+
+    # def test_is_model_unet_2d_2(self):
+    #
+    #     example_image_path = os.path.abspath(os.path.join(
+    #         base_path,
+    #         '../test_data/shapes/pixels/pixels_1.tiff'))
+    #
+    #     save_path = os.path.abspath(os.path.join(
+    #         base_path,
+    #         '../test_data/tmp/exported_model'))
+    #
+    #     model_path = os.path.join(
+    #         base_path,
+    #         '../test_data/tmp/model_convnet.h5')
+    #
+    #     print('model_path: {}'.format(model_path))
+    #
+    #     exp = DeepimagejExporter(model_path,
+    #                              save_path,
+    #                              example_image_path)
+    #
+    #     assert exp.is_model_unet_2d() is False
