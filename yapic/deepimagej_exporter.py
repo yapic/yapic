1a0377ee06e2e66a73d5b231b8f53ef89011d96b
moehlc <christoph.moehl@dzne.de>
2020-03-24 10:49:04 +0100

    added reshape_unet_2d to deepimagek_exporter

--- yapic/deepimagej_exporter.py
+++ yapic/deepimagej_exporter.py
@@ -1,4 +1,5 @@
 from yapic.session import Session
+from yapic.networks import unet_2d
 import os
 
 class DeepimagejExporter(object):
@@ -18,8 +19,10 @@ class DeepimagejExporter(object):
 
     def __init__(self, model_path, save_path, example_image_path):
 
-        save_dir = os.path.dirname(save_path)
-        assert os.path.isdir(save_dir), '{} does not exist'.format(save_dir)
+        self.save_dir = os.path.dirname(save_path)
+        msg = '{} does not exist'.format(self.save_dir)
+        assert os.path.isdir(self.save_dir), msg
+
         self.s = Session()
         self.s.load_prediction_data(example_image_path, 'some/path')
         self.s.load_model(model_path)
@@ -27,21 +30,30 @@ class DeepimagejExporter(object):
         msg = 'model is not unet_2d, cannot be exported to deepimagej'
         assert self.is_model_unet_2d(), msg
 
+        self.model_reshaped = None
+
     def is_model_unet_2d(self):
         return self.s.model.count_params() == 32424323
 
     def reshape_unet_2d(self, size='middle'):
-
-        if size=='small':
+        print(size)
+        if size == 'small':
             shape_xy = 112
-        if size=='middle':
+        elif size == 'middle':
+            print('yes')
             shape_xy = 224
-        if size=='large':
+        elif size == 'large':
             shape_xy = 448
         else:
             shape_xy = 112
+        print(shape_xy)
+        N_classes = self.s.model.output_shape[-1]
+        N_channels = self.s.model.input_shape[-1]
 
-        
-        model_reshaped = unet_2d.build_network(N_classes,
-                                              (N_channels, 1, small_xy, small_xy),
-                                               squeeze=True, padding='same')
+        self.model_reshaped = unet_2d.build_network(
+                             N_classes,
+                             (N_channels, 1, shape_xy, shape_xy),
+                             squeeze=True,
+                             padding='same')
+
+        self.model_reshaped.set_weights(self.s.model.get_weights())
