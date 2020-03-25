c846a409f4d678d1259b667b4775de9c30a1c6e1
moehlc <christoph.moehl@dzne.de>
2020-03-25 17:22:56 +0100

    added conditional KERAS dependencies regarding to tensorflow version

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 import os
-
 from setuptools import setup, find_packages
+
+try:
+    import tensorflow as tf
+except ModuleNotFoundError:
+    msg = ('install tensorflow version 1.12, 1.15 or 2.x '
+           'before installing YAPiC')
+    raise ModuleNotFoundError(msg)
+
+
 
 reqs = ['yapic_io>=0.1.0',
         'docopt>=0.6.2',
-        'numpy>=1.15.4',
-        'Keras>=2.3.0']
+        'numpy>=1.15.4']
 
+tf_version = [int(num) for num in tf.__version__.split('.')]
+
+if tf_version[0] >= 2:
+    reqs.append('Keras>=2.3.0')
+else:
+    if tf_version[0] == 1:
+        if tf_version[1] == 12:
+            # tensorflow==1.12
+            reqs.append('Keras==2.2.4')
+        elif tf_version[1] == 15:
+            # tensorflow==1.15
+            reqs.append('Keras>=2.3.0')
+        else:
+            msg = 'incompatible tensorflow version, use 1.12, 1.15 or 2.x'
+            raise Exception(msg)
 
 def readme():
     README_md = os.path.join(os.path.dirname(__file__), 'README.md')
--- yapic/tests/test_exporter.py
+++ yapic/tests/test_exporter.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from yapic.deepimagej_exporter import DeepimagejExporter
 from yapic.session import Session
 import os
+import shutil
 
 base_path = os.path.dirname(__file__)
 
@@ -62,6 +63,13 @@ class TestDeepimagejExporter(TestCase):
 
     @classmethod
     def setUpClass(cls):
+
+        # delete test artifacts
+        savepath = os.path.join(
+            base_path,
+            '../test_data/tmp')
+        shutil.rmtree(savepath, ignore_errors=True)
+
         model_path = train_test_model_unet_2d()
         print('saved unet_2d as {}'.format(model_path))
 
@@ -88,7 +96,7 @@ class TestDeepimagejExporter(TestCase):
                                  save_path,
                                  example_image_path)
 
-        assert exp.is_model_unet_2d()
+        assert exp._is_model_unet_2d()
 
     def test_reshape_unet_2d(self):
 
@@ -110,17 +118,44 @@ class TestDeepimagejExporter(TestCase):
                                  save_path,
                                  example_image_path)
 
-        exp.reshape_unet_2d()
+        exp._reshape_unet_2d()
         assert exp.model_reshaped.input_shape == (None, 224, 224, 1)
         assert exp.model_reshaped.output_shape == (None, 224, 224, 3)
 
-        exp.reshape_unet_2d(size='small')
+        exp._reshape_unet_2d(size='small')
         assert exp.model_reshaped.input_shape == (None, 112, 112, 1)
         assert exp.model_reshaped.output_shape == (None, 112, 112, 3)
 
-        exp.reshape_unet_2d(size='large')
-        assert exp.model_reshaped.input_shape == (None, 448, 448, 1)
-        assert exp.model_reshaped.output_shape == (None, 448, 448, 3)
+        exp._reshape_unet_2d(size='large')
+        assert exp.model_reshaped.input_shape == (None, 368, 368, 1)
+        assert exp.model_reshaped.output_shape == (None, 368, 368, 3)
+
+    def test_export_as_tensorflow_model(self):
+
+        example_image_path = os.path.abspath(os.path.join(
+            base_path,
+            '../test_data/shapes/pixels/pixels_1.tiff'))
+
+        save_path = os.path.abspath(os.path.join(
+            base_path,
+            '../test_data/tmp/exported_model'))
+
+        model_path = os.path.join(
+            base_path,
+            '../test_data/tmp/model_unet_2d.h5')
+
+        print('model_path: {}'.format(model_path))
+
+        exp = DeepimagejExporter(model_path,
+                                 save_path,
+                                 example_image_path)
+
+        exp._reshape_unet_2d(size='large')
+        exp._export_as_tensorflow_model()
+
+
+
+
 
 
 
