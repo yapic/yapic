07ca755701f7931ed15df0b495b6daa44111f457
cmohl2013 <cmohl@yahoo.com>
2020-10-15 18:08:19 +0200

    removed keras dependency and updated version in setup.py

--- setup.py
+++ setup.py
@@ -14,23 +14,6 @@ reqs = ['yapic_io>=0.1.0',
         'docopt>=0.6.2',
         'numpy>=1.15.4']
 
-tf_version = [int(num) for num in tf.__version__.split('.')]
-
-if tf_version[0] == 2 and tf_version[1] == 1:
-    reqs.append('Keras>=2.3.0')
-
-elif tf_version[0] == 1:
-    if tf_version[1] == 12:
-        # tensorflow==1.12
-        reqs.append('Keras==2.2.4')
-    else:
-        # tensorflow==1.12, 1.13, 1.14 or 1.15
-        reqs.append('Keras>=2.3.0')
-else:
-    msg = ('incompatible tensorflow version, '
-           'use 1.12, 1.13, 1.14, 1.15 or 2.1')
-    raise Exception(msg)
-
 
 def readme():
     README_md = os.path.join(os.path.dirname(__file__), 'README.md')
@@ -41,11 +24,13 @@ def readme():
 ns = os.environ.get('CI_PROJECT_NAMESPACE', 'idaf')
 
 setup(name='yapic',
-      version='1.0.2',
+      version='1.2.0',
       description='Yet another Pixel Classifier (based on deep learning)',
       long_description=readme(),
+      url='https://yapic.github.io/yapic/',
       author='Manuel Schoelling, Christoph Moehl',
-      author_email='manuel.schoelling@gmx.de, christoph.moehl@dzne.de',
+      author_email=('manuel.schoelling@gmx.de, ')
+                   ('christoph.oliver.moehl@gmail.com'),
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
       zip_safe=False,
       install_requires=reqs,
