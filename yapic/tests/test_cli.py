4c2e037336f56a181c0e947049f6c62f8040682c
Christoph Moehl <christoph.oliver.moehl@gmail.com>
2022-07-08 11:55:10 +0200

    added leaves example end2end training test  (#57)
    * wip added leaves example end2end training test
    * changed tensorflow version in travis config
    * disabled test and fixed installation instructions in docs

---
+++ yapic/tests/test_cli.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import zipfile
+import os
+import pytest
+from pytest_console_scripts import ScriptRunner
+
+
+@pytest.fixture
+def leaves_example_data_path(tmp_path):
+    zip_data_path = Path(__file__).parent.parent.parent / \
+        'docs/example_data/leaves_example_data.zip'
+    assert zip_data_path.exists()
+    with zipfile.ZipFile(zip_data_path, 'r') as zip_ref:
+        zip_ref.extractall(tmp_path)
+    return tmp_path
+
+@pytest.mark.skip('ilp label files are not supported in current version')
+def test_leafexample_train(leaves_example_data_path, script_runner: ScriptRunner):
+    # "'{}/*.tif'".format(str(leaves_example_data_path))
+    image_path = str(leaves_example_data_path)
+    label_path = "{}/leaf_labels_ilastik133.ilp".format(
+        str(leaves_example_data_path))
+    assert Path(label_path).exists()
+
+    ret = script_runner.run('yapic', 'train', 'unet_2d',
+                            image_path, label_path, '-e', '1', '--steps', '2')
+
+    assert ret.success
